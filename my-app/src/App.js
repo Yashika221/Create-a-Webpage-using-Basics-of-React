3e6a7cb2165741593a6cf42f6d9b21b8932b7202
Yashika Bhardwaj <yashub073@gmail.com>
2024-05-26 20:20:07 +0530

    This is my first webpage that I created with basic knowledge of React Js in this I follow 4 steps to create a new project in VS code using npm : step-1  npm create-react-app mynotesapp step-2  npm init react-app mynotesapp step-3  cd mynotesapp step-4 npm start after this we create all inbuild modules and packages of react and start this project on development mode

---
+++ my-app/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <img className="logo"src="./homelogo.png"/>
+      <p id='p1'>Your Dream Home is our Responsibility</p>
+      <h1>Welcomes To Sharma Real Estate</h1>
+      <img className="image1"src="https://media.istockphoto.com/id/1165384568/photo/europe-modern-complex-of-residential-buildings.jpg?s=612x612&w=0&k=20&c=iW4NBiMPKEuvaA7h8wIsPHikhS64eR-5EVPfjQ9GPOA="/>
+      <h3>Some Quotes</h3>
+      <p><span>“A good real estate agent sells himself before he sells his services.”</span></p>
+      <p><span>“A customer oriented realtor not only finds you a good home, he also finds you a good neighborhood.”</span></p>
+      <img className="image2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLgwCFEpaFeObw7lakhg1KaAI-Y7YfQhlTinmSW4npQA&s"/>
+      <h3>We Provide you following sevices</h3>
+      <ul>
+        <li>Property Valuation: Real estate agents help sellers determine the market value of their property through comparative market analysis and other valuation methods. </li>
+        <li>Listing and Marketing: Agents help sellers list their properties on the market, create marketing materials, and promote the property to potential buyers through various channels.</li>
+        <li>Property Search: For buyers, real estate agents assist in identifying properties that meet their criteria, arranging property viewings, and providing information on the local market.</li>
+        <li>Negotiation: Real estate agents negotiate on behalf of their clients to secure the best possible price and terms for a property sale or purchase.</li>
+        <li>Contracts and Paperwork: Agents handle the preparation and review of contracts, agreements, and other paperwork involved in real estate transactions to ensure compliance with legal requirements.</li>
+
+      </ul>
+      <img className="image1"src="https://homezonline.in/wp-content/uploads/2022/07/Beautiful-flat-roofing-duplex-home.jpeg"/>
+      <p id='p2'>Contact Us For More Details</p>
+      <p id='p3'>Contact Details:-
+        <ul >
+          <li>Name: Mr.Sarwan Kumar Sharma</li>
+          <li>Contact number: 9812103177 / 9541403177</li>
+          <li>Office Address: ADC Colony Satluj School Street, Near Chautala Residence ,Sirsa(Haryana)</li>
+          <li><a href="https://maps.app.goo.gl/xaJz2P4144MLjMjH8">Location📍</a></li>
+        </ul>
+        </p>
+        <h4>Please Visit Here🙏</h4>
+      
+    </div>
+  );
+       
+  
+}
+
+export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// if we want to install all packages or necessary modules for react project
+//using npm then we use (npm create vitelatest)after this we give name of project for e.g.(my-app)
+
+reportWebVitals();
